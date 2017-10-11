03dfa8b5345856a3e8fb348e4f68985c30cf1336
Hoon Lee <yl.hoony@gmail.com>
2017-10-11 09:13:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+function produceDrivingRange(blockRange) {
+  return function calculate(x, y) {
+    const blockX = parseInt(x.slice(0, 2));
+    const blockY = parseInt(y.slice(0, 2));
+    // console.log(typeof blockX, blockY);
+    let blockDistance = Math.abs(blockX - blockY);
+    let blockDiff = Math.abs(blockRange - blockDistance);
+    if (blockRange > blockDistance) {
+      return `within range by ${blockDiff}`;
+    } else {
+      return `${blockDiff} blocks out of range`;
+    }
+  }
+}
